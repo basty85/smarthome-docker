7363b40ed46751d7e86498f49c725359f45134ef
Sebastian Meyer <41087043+Basty85@users.noreply.github.com>
2026-01-07 22:44:52 +0100

    update docker OH setup

--- homegear/data/db.sql
+++ homegear/data/db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:066705a8b8e808efea48504a7e7ff1fd3fea4a7f2d0380b795705ef9cc9ec6fb
+oid sha256:e18e38e54571fb3e166b3065b0e8ec6be7d94930da3d8c3351451c99adb9812a
 size 425984
